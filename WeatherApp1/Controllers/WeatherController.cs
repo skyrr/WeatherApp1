f102bc51198902eacffc2082ffee375a88aaa4e3
vnebo <volodymyr.neborachko@zealicsolutions.com>
2018-10-02 12:24:52 +0300

    api works

---
+++ WeatherApp1/Controllers/WeatherController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace WeatherApp1.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Weather")]
+    public class WeatherController : Controller
+    {
+        public static IConfigurationRoot Configuration;
+        // GET api/values/5
+        [HttpGet("forecast/{city}")]
+        public IActionResult ForecastByCity(string city, string searchType)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+            var apiKey = Configuration["apiKey"];
+            string cityCodeLeipzig = "6548737";
+            HttpWebRequest apiRequest = WebRequest.Create("http://api.openweathermap.org/data/2.5/weather?id=" + city + "&appid=" + apiKey + "&units=metric") as HttpWebRequest;
+
+            string apiResponse = "";
+            using (HttpWebResponse response = apiRequest.GetResponse() as HttpWebResponse)
+            {
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                apiResponse = reader.ReadToEnd();
+            }
+
+            return Content(apiResponse);
+        }
+    }
+}
